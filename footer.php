680ce8ee34ab197b9d52f5863c2497c9805eb208
Connor Hansen <connorhansen@github.com>
2019-03-13 20:22:47 -0400

    Updated footer link styling

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
 <footer id="custom-footer">
-    <a href="index.php" class="footer-link">Home </a> <span class="footer-split">|</span>
-    <a href="about.php" class="footer-link"> About </a> <span class="footer-split">|</span>
-    <a href="contact.php" class="footer-link"> Contact </a> <span class="footer-split">|</span>
-    <a href="pricing.php" class="footer-link"> Pricing </a> <span class="footer-split">|</span>
-    <a href="faq.php" class="footer-link"> FAQ</a>
+    <a href="index.php" class="btn btn-link">Home </a> <span class="footer-split">|</span>
+    <a href="about.php" class="btn btn-link"> About </a> <span class="footer-split">|</span>
+    <a href="contact.php" class="btn btn-link"> Contact </a> <span class="footer-split">|</span>
+    <a href="pricing.php" class="btn btn-link"> Pricing </a> <span class="footer-split">|</span>
+    <a href="faq.php" class="btn btn-link"> FAQ</a>
 
     <hr>
 
